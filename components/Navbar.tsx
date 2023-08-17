2f4dba12cdf04c816e8080db3b09eb0783107f77
Nahuel Luca <nahuelluca20@gmail.com>
2023-08-17 20:48:20 -0300

    change: navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import Link from "next/link";
 
+import {Input} from "@/components/ui/input";
 import {cn} from "@/lib/utils";
 import {Icons} from "@/components/icons";
 import {
@@ -15,96 +16,23 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
-const components: {title: string; href: string; description: string}[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description: "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
-
 export function Navbar() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
+    <NavigationMenu className="py-4 px-10 border-b-[3px] mb-10 gap-3 md:gap-10 justify-between max-w-full ">
+      <NavigationMenuList className="gap-10">
+        <Link legacyBehavior passHref href="/">
+          <h1 className="font-bold cursor-pointer">EventsCalendar</h1>
+        </Link>
         <NavigationMenuItem>
-          <Link legacyBehavior passHref href="/docs">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              <h1 className="font-bold">Events</h1>
-            </NavigationMenuLink>
+          <Link legacyBehavior passHref href="/events">
+            events
           </Link>
         </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <Icons.logo className="h-6 w-6" />
-                    <div className="mb-2 mt-4 text-lg font-medium">shadcn/ui</div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components built with Radix UI and Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
+          <Input className="border-2" placeholder="search" />
         </NavigationMenuItem>
-        {/* <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem key={component.title} href={component.href} title={component.title}>
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem> */}
       </NavigationMenuList>
     </NavigationMenu>
   );
