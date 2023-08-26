66d85efd3a5c9b1f503a9a0e648deb8931dffaf4
Nahuel Luca <nahuelluca20@gmail.com>
2023-08-26 18:37:12 -0300

    add: calendar in single event page

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,11 +1,24 @@
 import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
 import {cookies} from "next/headers";
+import {format, parse, parseISO} from "date-fns";
+
+import {Calendar} from "@/components/ui/calendar";
 
 export default async function EventView({params: {id}}: {params: {id: string}}) {
   const supabase = createServerComponentClient<Database>({cookies});
   const {data} = await supabase.from("events").select().match({id}).single();
 
-  console.log(data);
+  const selectedDate = data?.date_event ? parseISO(data?.date_event) : undefined;
 
-  return <div>esto es id: {id}</div>;
+  return (
+    <div>
+      esto es id: {id}
+      <Calendar
+        className="rounded-md border"
+        defaultMonth={selectedDate}
+        mode="single"
+        selected={selectedDate}
+      />
+    </div>
+  );
 }
